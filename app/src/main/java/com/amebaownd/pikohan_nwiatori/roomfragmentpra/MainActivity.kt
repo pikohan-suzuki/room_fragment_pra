cf4a86fe017ad863820c0b376974e84108513db3
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-22 12:58:45 +0900

    完成品

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/roomfragmentpra/MainActivity.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/roomfragmentpra/MainActivity.kt
@@ -2,11 +2,30 @@ package com.amebaownd.pikohan_nwiatori.roomfragmentpra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstButton = findViewById<Button>(R.id.first_button)
+        firstButton.setOnClickListener {
+            val firstFragment = FirstFragment()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.container,firstFragment)
+            transaction.commit()
+        }
+
+        val secondButton = findViewById<Button>(R.id.second_button)
+        secondButton.setOnClickListener {
+            val secondFragment = SecondFragment()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.container,secondFragment)
+            transaction.commit()
+        }
     }
 }
